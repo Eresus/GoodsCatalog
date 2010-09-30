36949f1910b67ef2d754faed3eb289c0e7b6cad8
Михаил Красильников <m.krasilnikov@yandex.ru>
2010-09-30 13:28:55 +0000

    [ИСПРАВЛЕНО] ERES-184

--- src/goodscatalog/classes/GoodsClientUI.php
+++ src/goodscatalog/classes/GoodsClientUI.php
@@ -91,7 +91,7 @@ extends GoodsCatalogAbstractUI
 		$startFrom = ($pg - 1) * $maxCount;
 
 		$data['goods'] = GoodsCatalogGood::find($page->id, $maxCount, $startFrom, true);
-		$totalPages = ceil(GoodsCatalogGood::count($page->id) / $maxCount);
+		$totalPages = ceil(GoodsCatalogGood::count($page->id) / $maxCount, true);
 
 		if ($pg > $totalPages)
 		{
